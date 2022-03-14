af3f3283876ebaffc63a58cdcb78adcb3f3e7ef1
anggaraptra <kadekanggaraptra@gmail.com>
2022-03-14 21:48:22 +0800

    logout

--- functions/logout.php
+++ functions/logout.php
@@ -9,5 +9,5 @@ session_destroy();
 setcookie('id', '', time() - 3600, '/', 'localhost', 1);
 setcookie('key', '', time() - 3600, '/', 'localhost', 1);
 
-header("Location: ../login.php");
+header("Location: ../index.php");
 exit;
